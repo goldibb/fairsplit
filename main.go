8aa0057508ea73ffbe097ecf130d6c13f59f7d14
igor <59234118+goldibb@users.noreply.github.com>
2025-03-24 00:24:30 +0100

    update go.mod and go.sum to add new indirect dependencies; enhance main.go with database connection setup

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
+	"os"
+
 	"net/http"
 )
 
 func main() {
+	//Database connection
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+	//Routing
 	router := gin.Default()
-
 	router.Use(static.Serve("/", static.LocalFile("./client/build", true)))
-
 	api := router.Group("/api")
 	{
 		api.GET("/", func(c *gin.Context) {
